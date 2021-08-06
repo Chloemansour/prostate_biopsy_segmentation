1c3981bcfcf4e5d7026bb3687b8c45aa58028817
Chloemansour <16cgm6@queensu.ca>
2021-08-05 22:06:33 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+### BMIF804 - Mini Project
+### Author: Chloe Mansour
+### Student Number: 20061726
+### Date: August 7th, 2021
+
+### Description of File:
+
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
