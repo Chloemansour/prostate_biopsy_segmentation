188678bee287244c5956b29a36993187bfeef034
Chloemansour <16cgm6@queensu.ca>
2021-08-06 17:13:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,43 @@ import utils as ut
 
 # read in image
 MRI_volume = sitk.ReadImage("Case11.mhd")
-
+given_mask = sitk.ReadImage("Case11_segmentation.mhd")
+# Preform image preprocessing
 # print meta data
 print(MRI_volume.GetSize())
 print(MRI_volume.GetSpacing())
 print(MRI_volume.GetPixelIDTypeAsString())
 
+# max and min pixel values
+pixel_filter = sitk.StatisticsImageFilter()
+pixel_filter.Execute(MRI_volume)
 
-# create mask
+print("Min pixel value:", pixel_filter.GetMinimum())
+print("Max pixel value:", pixel_filter.GetMaximum())
+
+# visualize MRI volume image pixel intensities
+plt.hist(sitk.GetArrayFromImage(MRI_volume).flatten(), bins=70)
+plt.show()
+
+# rescale intensity to 0 - 255
+
+# MRI_volume = sitk.RescaleIntensity(MRI_volume, 0, 255)
+# MRI_volume = sitk.Cast(MRI_volume, sitk.sitkUInt8)
+
+# extract middle slice on LP plane
+size = MRI_volume.GetSize()
+z = (0, size[2]/2)
+
+# plot data
+plt.figure(figsize=(5,5))
+plt.imshow(sitk.GetArrayFromImage(MRI_volume)[:,:,22], cmap = "gray")
+plt.axis('off')
+plt.show()
+
+# seed points
+seeds = []
+
+'''# create mask
 prostate_mask = ut.prostate_segmenter(MRI_volume)
 
 # write mask to file
@@ -33,5 +62,11 @@ sitk.WriteImage(prostate_mask, "my_segmentation.nrrd")
 img_overlap = sitk.LabelOverlay(MRI_volume, prostate_mask)
 
 plt.figure(figsize=(15,15))
-plt.imshow(sitk.GetArrayFromImage(img_overlap))
+plt.imshow(sitk.GetArrayFromImage(img_overlap))'''
 
+# overlay provided mask onto image and view 2D LP slice
+
+given_overlap = sitk.LabelOverlay(MRI_volume, given_mask)
+
+plt.figure(figsize=(15,15))
+plt.imshow(sitk.GetArrayFromImage(given_overlap)[:,:,z])
