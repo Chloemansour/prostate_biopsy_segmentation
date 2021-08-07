ea99b6cd280c0c41476d721195b3c1cf6955108b
Chloemansour <16cgm6@queensu.ca>
2021-08-07 18:47:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import utils as ut
 
-# read in image
+ex_viewer = sitk.ImageViewer()
+slicer_location = "C:/Users/chloe/AppData/Local/NA-MIC/Slicer 4.11.20210226/Slicer.exe"
+ex_viewer.SetApplication(slicer_location)
+
+# read in image and segment
 MRI_volume = sitk.ReadImage("Case11.mhd")
 given_mask = sitk.ReadImage("Case11_segmentation.mhd")
 
@@ -66,25 +70,78 @@ plt.show()
 seeds = [MRI_volume.TransformPhysicalPointToIndex((-16.965, -25.925, 13.076)),
          MRI_volume.TransformPhysicalPointToIndex((-0.661, -0.569, 17.104)),
          MRI_volume.TransformPhysicalPointToIndex((-30.940, -2.501, 18.257)),
-         MRI_volume.TransformPhysicalPointToIndex((-35.366, -11.534, 16.008))]
+         MRI_volume.TransformPhysicalPointToIndex((-35.366, -11.534, 16.008)),
+         MRI_volume.TransformPhysicalPointToIndex((-15.800, -6.391, 16.342)),
+         MRI_volume.TransformPhysicalPointToIndex((-18.363, -19.968, 14.120)),
+         MRI_volume.TransformPhysicalPointToIndex((-29.554,-6.220, 16.747)),
+         MRI_volume.TransformPhysicalPointToIndex((-38.414,-0.970,17.875)),
+         MRI_volume.TransformPhysicalPointToIndex((-27.456,-19.319,14.478)),
+         MRI_volume.TransformPhysicalPointToIndex((-0.177, -4.018, 16.316))]
+
+
+'''feature_img = sitk.GradientMagnitudeRecursiveGaussian(MRI_volume, sigma=1.5)
+speed_img = sitk.BoundedReciprocal(feature_img)
+
+fm_filter = sitk.FastMarchingBaseImageFilter()
+fm_filter.SetTrialPoints(seeds)
+fm_filter.SetStoppingValue(500)
+seg = fm_filter.Execute(speed_img)
+
+thres = sitk.Threshold(seg, lower=0.0, upper=fm_filter.GetStoppingValue(),
+                       outsideValue=fm_filter.GetStoppingValue()+1)
+
+thres = sitk.Cast(thres, sitk.sitkInt8)
+prostate_mask = thres[:,:,z]'''
+
+'''plt.imshow(sitk.GetArrayFromImage(thres_img), vmin=0, vmax = 255, cmap = "gray", extent = [x0,x1,y0,y1])
+plt.show()'''
 
 # create mask
-prostate_mask = ut.prostate_segmenter(MRI_volume, seeds, 800, 1600)
+#prostate_mask = ut.prostate_segmenter(MRI_volume, seeds, 800, 1600)
 
 # write mask to file
-#sitk.WriteImage(prostate_mask, "my_segmentation.nrrd")
+#sitk.WriteImage(thres, "my_segmentation.nrrd")
 
 # overlay mask onto image and view 2D LP slice
-img_overlap = sitk.LabelOverlay(MRI_volume, prostate_mask)
-img_overlap = img_overlap[:,:,z]
+#img_overlap = sitk.LabelOverlay(MRI_volume,thres)
+#ex_viewer.Execute(img_overlap)
+# view middle 2D LP slice
+'''img_overlap = img_overlap[:,:,z]
 plt.figure(figsize=(15,15))
 plt.imshow(sitk.GetArrayFromImage(img_overlap), vmin=0, vmax = 255,  extent = [x0,x1,y0,y1])
-plt.show()
-# overlay provided mask onto image and view 2D LP slice
+plt.show()'''
 
+# overlay provided mask onto image and view 2D LP slice
 given_overlap = sitk.LabelOverlay(MRI_volume, given_mask)
-img_given =  given_overlap[:,:,z]
+
+# view middle 2D LP slice
+img_given = given_overlap[:,:,z]
 plt.figure(figsize=(15,15))
-plt.imshow(sitk.GetArrayFromImage(img_given), vmin=0, vmax = 255,  extent = [x0,x1,y0,y1])
+plt.imshow(sitk.GetArrayFromImage(img_given), vmin=0, vmax=255,  extent = [x0,x1,y0,y1])
 plt.axis('off')
 plt.show()
+
+area_dict = {}
+
+for s in range(10,32):
+    img = given_mask[:,:,s]
+    overlay_stats = sitk.LabelShapeStatisticsImageFilter()
+    overlay_stats.Execute(img)
+    overlay_area = overlay_stats.GetPhysicalSize(1)
+    area_dict[s] = overlay_area
+
+print(f"The image slide with the largest surface area of {max(area_dict.values())} is the image slice {max(area_dict,key=area_dict.get)}")
+
+# image with largest
+thick_mask = given_mask[:, :, max(area_dict, key=area_dict.get)]
+
+# get centroid point in LPS
+centroid = ut.get_target_loc(thick_mask)
+print("The centroid point to preform the biopsy is at:", centroid)
+
+centroid_idx = thick_mask.TransformPhysicalPointToIndex(centroid)
+
+centroid_final = (centroid_idx[0],centroid_idx[1], max(area_dict, key=area_dict.get))
+centroid_final = given_mask.TransformIndexToPhysicalPoint(centroid_final)
+print(centroid_final)
+
