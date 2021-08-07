fa70577c9ba4fa8671719cca03ae466d2afb1001
Chloemansour <16cgm6@queensu.ca>
2021-08-07 10:37:44 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,6 +12,14 @@ import pandas as pd
 
 
 def prostate_segmenter(volumetric_data, seed, lower, upper):
+    '''
+
+    :param volumetric_data:
+    :param seed:
+    :param lower:
+    :param upper:
+    :return:
+    '''
 
     connected_filter = sitk.ConnectedThresholdImageFilter()
     connected_filter.SetSeedList(seed)
@@ -24,20 +32,46 @@ def prostate_segmenter(volumetric_data, seed, lower, upper):
 
 
 def seg_eval_dice(ref_mask, mask):
+    '''
+
+    :param ref_mask:
+    :param mask:
+    :return:
+    '''
     dice_coefficient = sitk.LabelOverlapMeasuresImageFilter()
     dice_coefficient.GetDiceCoefficient(ref_mask, mask)
 
     return dice_coefficient.Execute(ref_mask, mask)
 
 def seg_eval_hausdorff(ref_mask, mask):
+    '''
+
+    :param ref_mask:
+    :param mask:
+    :return:
+    '''
+
     haus_filter = sitk.HausdorffDistanceImageFilter()
     haus_filter.GetHausdorffDistance(ref_mask,mask)
 
     return haus_filter.Execute(ref_mask, mask)
 
-def get_target_loc(mask):
-    pass
+def get_target_loc(ref_mask):
+    '''
 
+    :param ref_mask:
+    :return:
+    '''
 
-def pixel_extract(img, width):
+    x = [points[0] for points in ref_mask]
+    y = [points[1] for points in ref_mask]
+
+    centroid_mask = (sum(x) / len(ref_mask),
+                     sum(y) / len(ref_mask))
+
+    centroid_mask = (centroid_mask[0].TransformIndexToPhysicalPoint, centroid_mask[1].TransformIndexToPhysicalPoint)
+
+    return centroid_mask
+
+def pixel_extract(img, point, width):
     pass
